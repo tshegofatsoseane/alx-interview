c0619385031dc1b41a4dcfc3f640bb49e39e59be
tshegofatsoseane <tshegofatsoseane@gmail.com>
2024-03-08 02:19:52 +0000

    Fixed code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,11 +3,11 @@
 0. Pascal's Triangle
 """
 
-def pascal_triangle(n):
-    """Creates a new function def pascal_triangle(n): which returns a list of lists
-    with integers representing Pascal's triangle
-    """
 
+def pascal_triangle(n):
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
     res = []
     if n > 0:
         for i in range(1, n + 1):
