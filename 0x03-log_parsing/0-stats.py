0f9bccb4868b5ece8f4cb8361d97a61a33433814
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-03-26 23:36:33 +0000

    Task complete

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,13 +2,14 @@
 """
 log parsing
 """
+
 import sys
 import re
 
 
 def output(log: dict) -> None:
     """
-    A helper function to display stats
+    helper function to display stats
     """
     print("File size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
@@ -18,32 +19,35 @@ def output(log: dict) -> None:
 
 if __name__ == "__main__":
     regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+)\] "[A-Z]+ ([^"]+)" (\d+) (\d+)'
+    )
 
     line_count = 0
     log = {}
     log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+    log["code_frequency"] = {}
 
     try:
         for line in sys.stdin:
             line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
+            match = regex.match(line)
+            if match:
                 line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
+                method = match.group(2)
+                status_code = match.group(3)
+                file_size = int(match.group(4))
 
                 # File size
                 log["file_size"] += file_size
 
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
+                # Status code
+                log["code_frequency"].setdefault(status_code, 0)
+                log["code_frequency"][status_code] += 1
 
-                if (line_count % 10 == 0):
+                if line_count % 10 == 0:
                     output(log)
+    except KeyboardInterrupt:
+        pass  # Allow graceful exit if interrupted
     finally:
         output(log)
+
