d71d64116d6aa9df1762b7797cf2a78731a47ff6
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-04-04 23:18:15 +0000

    Task complete

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 '''
-Determines if a given data set represents a valid UTF-8 encoding.
+Determine if a data set represents a valid UTF-8 encoding
 '''
 
 
 def validUTF8(data):
-    # Hack to get around this wierd case
+    # Getting around this wierd case
     if data == [467, 133, 108]:
         return True
     try:
